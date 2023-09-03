6561c1cce3e63cf0128739bc25ffe29790170f9e
michaeldmx07 <michaeldmx07@gmail.com>
2023-09-03 04:37:14 -0500

    actualizado2

--- website/admin/bd.php
+++ website/admin/bd.php
@@ -1,16 +1,25 @@
 <?php
 
-$servidor = "localhost";
-$baseDeDatos = "website";
-$usuario = "root";
-$contrasenia = "";
+try {
+    $servidor = $_ENV['DB_HOST'];
+    $baseDeDatos = $_ENV['DB_NAME'];
+    $usuario = $_ENV['DB_USER'];
+    $contrasenia = $_ENV['DB_PASSWORD'];
+    $puerto = $_ENV['DB_PORT'];
 
-try{
-
-    $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $contrasenia);
-
-}catch(Exception $error){
-    echo $error ->getMessage();
+    $conexion = new PDO("mysql:host=$servidor;port=$puerto;dbname=$baseDeDatos", $usuario, $contrasenia);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $error) {
+    echo "Error de conexión: " . $error->getMessage();
 }
+
+// Verifica si la conexión se ha establecido correctamente
+if (!$conexion) {
+    die("Error: No se pudo conectar a la base de datos.");
+}
+
+// Aquí puedes realizar tus consultas y operaciones en la base de datos usando $conexion
+
+
 
 ?>
